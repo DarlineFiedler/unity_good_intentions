58accead746df683972fcb25938a97bbf3455763
Darline Fiedler <darline-fiedler@gmx.de>
2021-12-19 23:08:56 +0100

    -player movement -change lvl - not perfect

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -8,13 +8,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float runSpeed = 10f;
 
+    [SerializeField]
+    float jumpSpeed = 5f;
+
     Vector2 moveInput;
 
     Rigidbody2D myRigidbody;
 
+    CapsuleCollider2D myCapsuleCollider;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -25,7 +31,19 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log (moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            myRigidbody.velocity +=
+                new Vector2(myRigidbody.velocity.x, jumpSpeed);
+        }
     }
 
     void Run()
